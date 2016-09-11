740b993c1bf6a1a139cccbab98ac53a8d9df5003
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-11 20:29:40 -0300

    Corrige versionamento para releases

---
+++ dist/zttdesignlite.js
@@ -0,0 +1,55 @@
+(function () {
+  angular.module("zttDesignLite", []);
+})();
+
+(function () {
+  angular
+    .module("zttDesignLite")
+    .directive("zttInput", zttInput);
+
+  function zttInput () {
+    var directive = {
+      restrict: "A",
+      require: "ngModel",
+      link: linkFunction
+    };
+
+    function linkFunction (scope, element, attributes, ngModel) {
+      var formGroupFocusClass = "ztt-form-group--focus";
+      var formLabelFocusClass = "ztt-form-group__label--focus";
+
+      var input     = element[0];
+      var formGroup = input.parentElement;
+      var formLabel = formGroup.querySelector("label");
+
+      input.addEventListener("focusin", focusInHandler);
+      input.addEventListener("focusout", focusOutHandler);
+      input.addEventListener("input change blue", inputUpdateHandler);
+
+      scope.$watch(function () {
+        return ngModel.$modelValue;
+      }, inputUpdateHandler);
+
+      function focusInHandler(event) {
+        formGroup.classList.add(formGroupFocusClass);
+        formLabel.classList.add(formLabelFocusClass);
+      }
+
+      function focusOutHandler(event) {
+        formGroup.classList.remove(formGroupFocusClass);
+
+        if (input.value === "") {
+          formLabel.classList.remove(formLabelFocusClass);
+        }
+      }
+
+      function inputUpdateHandler(event) {
+        if (input.value !== "") {
+          formLabel.classList.add(formLabelFocusClass);
+        }
+      }
+    }
+
+    return directive;
+  };
+})();
