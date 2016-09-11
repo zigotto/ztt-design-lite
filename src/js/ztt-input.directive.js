96dc4488a3ae86af24466619d1cd46c07d79feb6
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-10 22:25:53 -0300

    Cria view para testes e pasta para criar arquivos js

---
+++ src/js/ztt-input.directive.js
@@ -0,0 +1,11 @@
+(function () {
+  angular
+    .module("zttDesignLite")
+    .directive("zttInput", zttInput);
+
+  function zttInput() {
+    var directive = {};
+
+    return directive;
+  };
+})();
