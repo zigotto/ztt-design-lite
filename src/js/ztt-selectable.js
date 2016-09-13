139aa4383647c0a63417fccb2329a53224377f6b
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-13 15:49:58 -0300

    Implementa melhoria no focus state dos controles de seleção

--- src/js/ztt-selectable.js
+++ src/js/ztt-selectable.js
@@ -1,8 +1,4 @@
 (function () {
-  angular.module("zttDesignLite", []);
-})();
-
-(function () {
   angular
     .module("zttDesignLite")
     .directive("zttSelectable", zttSelectable);
@@ -61,65 +57,4 @@
 
     return directive;
   }
-})();
-
-(function () {
-  angular
-    .module("zttDesignLite")
-    .directive("zttInput", zttInput);
-
-  function zttInput () {
-    var directive = {
-      restrict: "A",
-      require: "?ngModel",
-      link: linkFunction
-    };
-
-    function linkFunction (scope, element, attributes, ngModel) {
-      var formGroupFocusClass = "ztt-form-group__wrapper--focus";
-      var formLabelFocusClass = "ztt-form-group__label--focus";
-
-      var input     = element[0];
-      var formGroup = input.parentElement;
-      var formLabel = formGroup.querySelector("label");
-
-      var floatingLabel = attributes.zttInput !== "placeholder";
-
-      input.addEventListener("focusin", focusInHandler);
-      input.addEventListener("focusout", focusOutHandler);
-      input.addEventListener("input change blue", inputUpdateHandler);
-
-      if (ngModel) {
-        scope.$watch(function () {
-          return ngModel.$modelValue;
-        }, inputUpdateHandler);
-      }
-
-      function focusInHandler(event) {
-        formGroup.classList.add(formGroupFocusClass);
-
-        if (floatingLabel) {
-          formLabel.classList.add(formLabelFocusClass);
-        }
-      }
-
-      function focusOutHandler(event) {
-        formGroup.classList.remove(formGroupFocusClass);
-
-        if (input.value === "") {
-          formLabel.classList.remove(formLabelFocusClass);
-        }
-      }
-
-      function inputUpdateHandler(event) {
-        if (input.value.trim() !== "") {
-          formLabel.classList.add(formLabelFocusClass);
-        } else if (!floatingLabel) {
-          formLabel.classList.remove(formLabelFocusClass);
-        }
-      }
-    }
-
-    return directive;
-  };
 })();
